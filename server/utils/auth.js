9562403d7199a4be24f12c0d2a633548c4fcb014
Daniel <daniel-alh@hotmail.com>
2022-08-17 06:12:51 -0500

    modified server auth and server package json to update latest versions

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,28 +1,36 @@
+
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  // function for our authenticated routes
   authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    // if token is not found, then return the request object
     if (!token) {
       return req;
     }
 
+    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch (err) {
-      console.error(err);
+    } catch {
       console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
     }
 
+    // return the request object so it can be passed to the resolver as `context`
     return req;
   },
   signToken: function ({ username, email, _id }) {
